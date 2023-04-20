99ced09ebe6ad3e368f918d4d53c9aec7d90bd83
George <georgeworld@qq.com>
2023-04-20 21:28:56 +0800

    add excel number column style

---
+++ src/main/java/com/georgeinfo/excel/handler/NumberColumnSheetHandler.java
@@ -0,0 +1,27 @@
+package com.georgeinfo.excel.handler;
+
+import com.alibaba.excel.write.handler.SheetWriteHandler;
+import com.alibaba.excel.write.metadata.holder.WriteSheetHolder;
+import com.alibaba.excel.write.metadata.holder.WriteWorkbookHolder;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.DataFormat;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class DemoSheetHandler implements SheetWriteHandler {
+    @Override
+    public void beforeSheetCreate(WriteWorkbookHolder writeWorkbookHolder, WriteSheetHolder writeSheetHolder) {
+
+    }
+
+    @Override
+    public void afterSheetCreate(WriteWorkbookHolder writeWorkbookHolder, WriteSheetHolder writeSheetHolder) {
+        Workbook workbook = writeWorkbookHolder.getWorkbook();
+        Sheet sheet = workbook.getSheetAt(0);
+        CellStyle cs = workbook.createCellStyle();
+        DataFormat format = workbook.createDataFormat();
+        cs.setDataFormat(format.getFormat("#.#"));
+
+        sheet.setDefaultColumnStyle(2, cs);
+    }
+}
