3a08b82ebf9aaa078fb2ddc54cd6a6154c5ca431
George <georgeworld@qq.com>
2023-06-20 22:45:23 +0800

    增加了一行测试打印

--- src/test/java/test/gittest/IamGitTester.java
+++ src/test/java/test/gittest/IamGitTester.java
@@ -2,7 +2,7 @@ package test.gittest;
 
 import org.junit.Test;
 
-public class IAmGitTester {
+public class IamGitTester {
     @Test
     public void testGit(){
         System.out.println("我是来自分支test的提交");
