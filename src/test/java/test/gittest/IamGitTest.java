de77894cb0a17e75d055a2d85478f0cc3a555ecc
George <georgeworld@qq.com>
2023-06-20 23:12:37 +0800

    master分支提交4

--- src/test/java/test/gittest/IamGitTest.java
+++ src/test/java/test/gittest/IamGitTest.java
@@ -12,5 +12,6 @@ public class IamGitTest {
         System.out.println("我是来自分支test的提交3");
         System.out.println("我是来自分支test的提交4");
         System.out.println("我是来自分支master的提交3");
+        System.out.println("我是来自分支master的提交4");
     }
 }
