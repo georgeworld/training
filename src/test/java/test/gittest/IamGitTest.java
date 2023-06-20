03255ac8dc6a77a024474f02a7c442e2c693d7e0
George <georgeworld@qq.com>
2023-06-20 22:51:26 +0800

    测试git merge

--- src/test/java/test/gittest/IamGitTest.java
+++ src/test/java/test/gittest/IamGitTest.java
@@ -7,5 +7,6 @@ public class IamGitTest {
     public void testGit(){
         System.out.println("我是来自分支test的提交");
         System.out.println("我是来自分支master的提交");
+        System.out.println("我是来自分支test的提交2");
     }
 }
