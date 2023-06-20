296f68295859204d868d0a30d8de303cf381ff08
George <georgeworld@qq.com>
2023-06-20 22:49:32 +0800

    测试merge

--- src/test/java/test/gittest/IamGitTest.java
+++ src/test/java/test/gittest/IamGitTest.java
@@ -2,7 +2,7 @@ package test.gittest;
 
 import org.junit.Test;
 
-public class IamGitTester {
+public class IamGitTest {
     @Test
     public void testGit(){
         System.out.println("我是来自分支test的提交");
